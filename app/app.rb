7ad593ffba94cecfe468def093ea07239c9dddd9
Emma Koszinowski <emkosz@gmail.com>
2015-09-29 15:05:47 -0700

    Added a default app

--- app/app.rb
+++ app/app.rb
@@ -1,25 +1,6 @@
 require 'sinatra'
-require 'sequel'
 
-# hook up to the database
-DB = Sequel.sqlite :database => "stories.cocoapods.org.db"
-# DB = Sequel.postgres(:host=>'localhost', :user=>'user', :password=>'password',
-#       :database=>'stories_db')
-
-#create an items table (only if it doesn't exists)
-DB.create_table? :items do
-  primary_key :id
-  String :title, :size => 512
-  String :body, :text=>true
-end
-
-# create a dataset from the items table
-items = DB[items]
-
+# default app
 get '/' do
   'hello world'
-end
-
-get '/user' do
-  'hello emma'
 end
