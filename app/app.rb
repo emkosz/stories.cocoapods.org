e8167664aa7c9386dba64af3b503fb96b546a5b6
Emma Koszinowski <emkosz@gmail.com>
2015-09-28 17:13:26 -0700

    Database hookup

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'sequel'
+
+# hook up to the database
+DB = Sequel.sqlite :database => "stories.cocoapods.org.db"
+# DB = Sequel.postgres(:host=>'localhost', :user=>'user', :password=>'password',
+#       :database=>'stories_db')
+
+#create an items table (only if it doesn't exists)
+DB.create_table? :items do
+  primary_key :id
+  String :title, :size => 512
+  String :body, :text=>true
+end
+
+# create a dataset from the items table
+items = DB[items]
 
 get '/' do
   'hello world'
+end
+
+get '/user' do
+  'hello emma'
 end
