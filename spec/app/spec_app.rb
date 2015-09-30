cd5adac6eef74093b37bb5eda03af31e26785a9d
Emma Koszinowski <emkosz@gmail.com>
2015-09-30 15:19:20 -0700

    Added test for the default app

--- spec/app/spec_app.rb
+++ spec/app/spec_app.rb
@@ -1,13 +1,14 @@
 require File.expand_path('../spec_helper' __FILE__)
-require File.expand_path('../app/app', __FILE__)
+require File.expand_path('../../app/app', __FILE__)
 
 
 describe Stories do
 
   it 'returns the right value' do
-    #given
-    #get '/'
-    #when
-    #then
+    get '/app/app'
+    last_response.status.should == 200
+    JSON.parse(last_response.body).should == {
+        'hello world'
+    }
   end
 end
